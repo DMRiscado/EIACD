0351f21d7708221c14fdb6d183b5b4fd45e8d36e
DMRiscado <145716667+DMRiscado@users.noreply.github.com>
2024-03-23 17:29:14 +0000

    Início de Análise
    Caminho estático deixado como comentário de modo a permitir o seu funcionamento. Alteração futura.
    
    Co-Authored-By: Tiago Marques <141578461+Sp4zie27@users.noreply.github.com>

--- analise_cidades/analise_comparacao_cidades.py
+++ analise_cidades/analise_comparacao_cidades.py
@@ -3,8 +3,21 @@ import numpy as np
 
 
 # importar dataset
-df_analise = pd.read_csv("datasetfiles/full_housing.csv")
+data_total= "datasetfiles\concatenated_housing.csv"
+#df_analise = pd.read_csv(r"C:\Users\Administrator\Documents\GitHub\EIACD\datasetfiles\concatenated_housing.csv")
 
 # mediana dos metros quarados disponiveis
 median_sqft = df_analise['sqft_living'].median()
 print("Mediana de metros quadrados disponíveis: ", median_sqft)
+
+# mediana dos preços
+median_price = df_analise['price'].median()
+print("Mediana dos preços: ", median_price)
+
+#média dos preços
+mean_price = df_analise['price'].mean()
+print("Média dos preços: ", mean_price)
+
+#média dos quartos disponíveis arredondada
+mean_bedrooms = round(df_analise['bedrooms'].mean())
+print("Média dos quartos disponíveis: ", mean_bedrooms)
