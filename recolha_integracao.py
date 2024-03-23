a2bdda0bc4aa8ca08900728d4e0b15dc92436699
DMRiscado <145716667+DMRiscado@users.noreply.github.com>
2024-03-23 16:47:18 +0000

    Concatenação Geral Concluída
    Co-Authored-By: Tiago Marques <141578461+Sp4zie27@users.noreply.github.com>

--- recolha_integracao.py
+++ recolha_integracao.py
@@ -17,7 +17,7 @@ df_perth = pd.read_csv(data_perth)
 #delhi_drop = ["Status", "neworold", "Furnished_status", "Lift", "Landmarks", "desc", "Unnamed: 0"]
 #df_delhi.drop(columns=delhi_drop, inplace=True)
 
-#melbourne_drop = ["Rooms", "Method", "SellerG", "Distance", "Landsize", "BuildingArea", "CouncilArea", "Regionname", "Propertycount", "Unnamed: 0"]
+#melbourne_drop = ["Rooms", "Method", "SellerG", "Distance", "BuildingArea", "CouncilArea", "Regionname", "Propertycount", "Unnamed: 0"]
 #df_melbourne.drop(columns=melbourne_drop, inplace=True)
 
 #perth_drop = ["CBD_DIST", "NEAREST_STN","NEAREST_STN_DIST", "NEAREST_SCH", "NEAREST_SCH_DIST", "NEAREST_SCH_RANK"]
@@ -40,6 +40,7 @@ df_delhi.rename(columns = {'Price_sqft':'price_sqft'}, inplace = True)
 
 # Renomear colunas Melbourne
 df_melbourne.rename(columns = {'Suburb':'suburb'}, inplace = True)
+df_melbourne.rename(columns = {'Landsize':'sqft_living'}, inplace = True)
 df_melbourne.rename(columns = {'Address':'address'}, inplace = True)
 df_melbourne.rename(columns = {'Type':'type'}, inplace = True)
 df_melbourne.rename(columns = {'Price':'price'}, inplace = True)
@@ -72,7 +73,7 @@ df_perth.rename(columns = {'LONGITUDE':'longitude'}, inplace = True)
 # Lista das colunas a serem convertidas para inteiros em cada DataFrame
 cols_to_convert_delhi = ['price', 'sqft_living', 'bedrooms', 'bathrooms','balcony', 'car_garage', 'price_sqft']
 cols_to_convert_perth = ['car_garage', 'year_built']
-cols_to_convert_melbourne = ['zipcode', 'bedrooms', 'bathrooms', 'car_garage', 'year_built']
+cols_to_convert_melbourne = ['zipcode', 'bedrooms', 'bathrooms', 'car_garage', 'year_built', 'sqft_living']
 
 for col in cols_to_convert_delhi:
     df_delhi[col] = pd.to_numeric(df_delhi[col], errors='coerce').astype('Int64')
@@ -87,8 +88,8 @@ for col in cols_to_convert_melbourne:
 # df_perth['date_sold'] = pd.to_datetime(df_perth['date_sold'], format= "%m/%Y")
 # df_perth['date_sold'] = df_perth['date_sold'].dt.strftime("%m/%Y")
 
-# df_melbourne['date_sold'] = pd.to_datetime(df_melbourne['date_sold'], format= "%d/%m/%Y")
-# df_melbourne['date_sold'] = df_melbourne['date_sold'].dt.strftime("%m/%Y")
+#df_melbourne['date_sold'] = pd.to_datetime(df_melbourne['date_sold'], format= "%d/%m/%Y")
+#df_melbourne['date_sold'] = df_melbourne['date_sold'].dt.strftime("%m/%Y")
 
 # Reescrever os arquivos originais com os DataFrames modificados
 df_delhi.to_csv(data_delhi, index=False)
@@ -96,11 +97,15 @@ df_melbourne.to_csv(data_melbourne, index=False)
 df_perth.to_csv(data_perth, index=False)
 
 
-# print(df_delhi.dtypes)
-# print(df_melbourne.dtypes)
-# print(df_perth.dtypes)
+#print(df_delhi.dtypes)
+#print(df_melbourne.dtypes)
+#print(df_perth.dtypes)
 
-# concatenar os datasets depois de apurados
+#Concatenar os datasets depois de apurados e gravá-los numa novo CSV
 
-#concatenated_df = pd.concat([df_delhi, df_melbourne, df_perth], ignore_index=True)
+concatenated_df = pd.concat([df_delhi, df_melbourne, df_perth], ignore_index=True)
+
+concatenated_df.to_csv("datasetfiles/concatenated_housing.csv", index=False)
+
+
 
