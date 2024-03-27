ba33497e66f709bd429709600be9daa6f59334f2
Sp4zie27 <sp4ziemarques2335@gmail.com>
2024-03-27 16:27:42 +0000

    limpeza melbourne and perth
    limpeza melbourne and perth
    
    Co-Authored-By: DMRiscado <145716667+DMRiscado@users.noreply.github.com>

--- pre_processamento_limpeza_melbourne.py
+++ pre_processamento_limpeza_melbourne.py
@@ -1,1 +1,26 @@
 import pandas as pd
+
+data_melbourne = "datasetfiles/melbourne_housing.csv"
+df_melbourne = pd.read_csv(data_melbourne)
+
+missing_values = df_melbourne.isnull().sum()
+print(missing_values)
+
+
+#def remover_linhas(df_melbourne, arquivo_csv):
+    #valores_falta = df_melbourne.isnull().sum(axis=1)
+    #indices_remover = valores_falta[valores_falta == 7].index
+    #df = df_melbourne.drop(indices_remover)
+    #df.to_csv(arquivo_csv, index=False)
+
+#remover_linhas(df_melbourne, 'datasetfiles/melbourne_housing.csv')
+
+
+#colunas_em_falta = ["bedrooms", "bathrooms", "car_garage", "sqft_living", "year_built", "latitude", "longitude"]
+
+#for col in df_melbourne.columns:
+    #if df_melbourne[col].dtype != 'object':
+        #median = df_melbourne[col].median()
+        #df_melbourne[col].fillna(median, inplace=True)
+
+#df_melbourne.to_csv('datasetfiles/melbourne_housing.csv', index=False)
--- pre_processamento_limpeza_perth.py
+++ pre_processamento_limpeza_perth.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+
+data_perth = "datasetfiles/perth_housing.csv"
+df_perth = pd.read_csv(data_perth)
+
+missing_values = df_perth.isnull().sum()
+print(missing_values)
+
+#colunas_em_falta = ["car_garage", "year_built "]
+
+#for col in df_perth.columns:
+    #if df_perth[col].dtype != 'object':
+        #median = df_perth[col].median()
+        #df_perth[col].fillna(median, inplace=True)
+
+#df_perth.to_csv('datasetfiles/perth_housing.csv', index=False)
