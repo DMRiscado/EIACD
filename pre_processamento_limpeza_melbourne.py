7a29214c88be0736d5b2ad6da8fbcb1fa3358e5d
Sp4zie27 <sp4ziemarques2335@gmail.com>
2024-03-28 11:33:04 +0000

    outliers melbourne and perth
    outliers melbourne and perth
    
    Co-Authored-By: DMRiscado <145716667+DMRiscado@users.noreply.github.com>

--- pre_processamento_limpeza_melbourne.py
+++ pre_processamento_limpeza_melbourne.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 data_melbourne = "datasetfiles/melbourne_housing.csv"
 df_melbourne = pd.read_csv(data_melbourne)
@@ -6,6 +7,7 @@ df_melbourne = pd.read_csv(data_melbourne)
 missing_values = df_melbourne.isnull().sum()
 print(missing_values)
 
+#Remoção de linhas com valores nulos
 
 #def remover_linhas(df_melbourne, arquivo_csv):
     #valores_falta = df_melbourne.isnull().sum(axis=1)
@@ -23,4 +25,59 @@ print(missing_values)
         #median = df_melbourne[col].median()
         #df_melbourne[col].fillna(median, inplace=True)
 
-#df_melbourne.to_csv('datasetfiles/melbourne_housing.csv', index=False)
+
+
+#Remoção de outliers usando a técnica IQR 5/95
+
+#Q1 = df_melbourne['year_built'].quantile(0.05)
+#Q3 = df_melbourne['year_built'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_melbourne[(df_melbourne['year_built'] >= Q1 - 1.5 * IQR) & (df_melbourne['year_built'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/melbourne_housing.csv', index=False)
+
+
+
+#Q1 = df_melbourne['sqft_living'].quantile(0.05)
+#Q3 = df_melbourne['sqft_living'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_melbourne[(df_melbourne['sqft_living'] >= Q1 - 1.5 * IQR) & (df_melbourne['sqft_living'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/melbourne_housing.csv', index=False)
+
+
+
+#Q1 = df_melbourne['bedrooms'].quantile(0.05)
+#Q3 = df_melbourne['bedrooms'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_melbourne[(df_melbourne['bedrooms'] >= Q1 - 1.5 * IQR) & (df_melbourne['bedrooms'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/melbourne_housing.csv', index=False)
+
+
+
+#Q1 = df_melbourne['bathrooms'].quantile(0.05)
+#Q3 = df_melbourne['bathrooms'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_melbourne[(df_melbourne['bathrooms'] >= Q1 - 1.5 * IQR) & (df_melbourne['bathrooms'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/melbourne_housing.csv', index=False)
+
+
+#Q1 = df_melbourne['car_garage'].quantile(0.05)
+#Q3 = df_melbourne['car_garage'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_melbourne[(df_melbourne['car_garage'] >= Q1 - 1.5 * IQR) & (df_melbourne['car_garage'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/melbourne_housing.csv', index=False)
+
+
+
+#Q1 = df_melbourne['price'].quantile(0.05)
+#Q3 = df_melbourne['price'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_melbourne[(df_melbourne['price'] >= Q1 - 1.5 * IQR) & (df_melbourne['price'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/melbourne_housing.csv', index=False)
+
+
+
+#Q1 = df_melbourne['zipcode'].quantile(0.05)
+#Q3 = df_melbourne['zipcode'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_melbourne[(df_melbourne['zipcode'] >= Q1 - 1.5 * IQR) & (df_melbourne['zipcode'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/melbourne_housing.csv', index=False)
--- pre_processamento_limpeza_perth.py
+++ pre_processamento_limpeza_perth.py
@@ -7,6 +7,9 @@ df_perth = pd.read_csv(data_perth)
 missing_values = df_perth.isnull().sum()
 print(missing_values)
 
+
+#Remoção de linhas com valores nulos
+
 #colunas_em_falta = ["car_garage", "year_built "]
 
 #for col in df_perth.columns:
@@ -15,3 +18,69 @@ print(missing_values)
         #df_perth[col].fillna(median, inplace=True)
 
 #df_perth.to_csv('datasetfiles/perth_housing.csv', index=False)
+
+
+
+#Remoção de outliers usando a técnica IQR 5/95
+
+#Q1 = df_perth['year_built'].quantile(0.05)
+#Q3 = df_perth['year_built'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_perth[(df_perth['year_built'] >= Q1 - 1.5 * IQR) & (df_perth['year_built'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/perth_housing.csv', index=False)
+
+
+
+#Q1 = df_perth['sqft_living'].quantile(0.05)
+# Q3 = df_perth['sqft_living'].quantile(0.95)
+# IQR = Q3 - Q1
+# outliers = df_perth[(df_perth['sqft_living'] >= Q1 - 1.5 * IQR) & (df_perth['sqft_living'] <= Q3 + 1.5 * IQR)]
+# outliers.to_csv('datasetfiles/melbourne_housing.csv', index=False)
+
+
+
+#Q1 = df_perth['sqft_lot'].quantile(0.05)
+# Q3 = df_perth['sqft_lot'].quantile(0.95)
+# IQR = Q3 - Q1
+# outliers = df_perth[(df_perth['sqft_lot'] >= Q1 - 1.5 * IQR) & (df_perth['sqft_lot'] <= Q3 + 1.5 * IQR)]
+# outliers.to_csv('datasetfiles/perth_housing.csv', index=False)
+
+
+
+#Q1 = df_perth['bedrooms'].quantile(0.05)
+#Q3 = df_perth['bedrooms'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_perth[(df_perth['bedrooms'] >= Q1 - 1.5 * IQR) & (df_perth['bedrooms'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/perth_housing.csv', index=False)
+
+
+
+#Q1 = df_perth['bathrooms'].quantile(0.05)
+#Q3 = df_perth['bathrooms'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_perth[(df_perth['bathrooms'] >= Q1 - 1.5 * IQR) & (df_perth['bathrooms'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/perth_housing.csv', index=False)
+
+
+
+#Q1 = df_perth['car_garage'].quantile(0.05)
+#Q3 = df_perth['car_garage'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_perth[(df_perth['car_garage'] >= Q1 - 1.5 * IQR) & (df_perth['car_garage'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/perth_housing.csv', index=False)
+
+
+
+#Q1 = df_perth['price'].quantile(0.05)
+#Q3 = df_perth['price'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_perth[(df_perth['price'] >= Q1 - 1.5 * IQR) & (df_perth['price'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/perth_housing.csv', index=False)
+
+
+
+#Q1 = df_perth['zipcode'].quantile(0.05)
+#Q3 = df_perth['zipcode'].quantile(0.95)
+#IQR = Q3 - Q1
+#outliers = df_perth[(df_perth['zipcode'] >= Q1 - 1.5 * IQR) & (df_perth['zipcode'] <= Q3 + 1.5 * IQR)]
+#outliers.to_csv('datasetfiles/perth_housing.csv', index=False)
